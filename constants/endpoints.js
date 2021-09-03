5f092654b8cff82cdfb520a60325a3ecdf4c096b
byilmaz <byilmaz@hotech.systems>
2021-09-03 20:12:50 +0300

    add scrip_tag endpoint to endpoints.js

--- constants/endpoints.js
+++ constants/endpoints.js
@@ -1,6 +1,7 @@
 export const ADMIN_API = '/admin/api'
 export const ASSETS_JSON = '/assets.json'
 export const PAGES_JSON = '/pages.json'
+export const SCRIPT_TAGS_JSON = '/script_tags.json'
 export const THEMES = '/themes'
 export const THEMES_JSON = '/themes.json'
 
